cc7e8db8749129fae6f0fda93520c927fbedd790
RasheedHD <rasheed200612@gmail.com>
2026-03-31 21:10:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-import requests
+import urllib.request
+import json
 
-user = input("> ")
-endpoint = f"https://api.github.com/users/{user}/events"
+#user = input("> ")
+url = f"https://api.github.com/users/{"RasheedHD"}/events"
 
-def fetch_user_activity(user):
-    pass
+def fetch_user_activity():
+    resp = urllib.request.urlopen(url)
+    data = resp.read()
+    decodedData = data.decode("UTF-8")
+    pythonList = json.loads(decodedData)
+    #print(pythonList)
+    for event in pythonList:
+        if event["type"] == "PushEvent":
+            print(f"- Pushed 1 commit to {event["repo"]["name"]}")
+            pass
+        elif event["type"] == "CreateEvent":
+            print(f"- Created repository {event["repo"]["name"]}")
+        elif event["type"] == "WatchEvent":
+            print(f"- Starred {event["repo"]["name"]}")
 
-fetch_user_activity(user)
+
+fetch_user_activity()
